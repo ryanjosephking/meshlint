4b843d6296ca9888f7d5da9b62ac559f6e62de70
rking <rking@panoptic.com>
2012-04-09 22:09:32 -0600

    Continuous Mode is almost Here!
    Really, this was another messy commit. I should have broken it down quite a
    bit. Some new things:
    
    - Add unittest!  It is mostly working perfectly. The only current problem is
      the single newline it prints, at the moment. Will have to debug further.
      Other than that, this is a cool new idea for Addons (IMO).
    
    - Using the unit tests, debugged the knotty diff_analyses() method.
    
    - Homogenize some names
    
    - Remove trailing '.'s from descriptions. For some reason this is a warning.

--- meshlint.py
+++ meshlint.py
@@ -4,7 +4,7 @@ bl_info = {
     'version': (0, 2),
     'blender': (2, 6, 3),
     'location': 'Object Data properties > MeshLint',
-    'description': 'Check a mesh for: Tris / Ngons / Nonmanifoldness / etc.',
+    'description': 'Check a mesh for: Tris / Ngons / Nonmanifoldness / etc',
     'warning': '',
     'wiki_url': '',
     'tracker_url': '',
@@ -48,7 +48,7 @@ CHECKS = [
   # [Your great new idea here] -> Tell me about it: rking@panoptic.com
 ]
 
-N_A_STR = '(N/A)'
+N_A_STR = '(N/A - disabled)'
 TBD_STR = '...'
 
 for lint in CHECKS:
@@ -69,7 +69,7 @@ class MeshLintAnalyzer:
         self.b = bmesh.from_edit_mesh(self.obj.data)
 
     def find_problems(self):
-        found = [] 
+        analysis = [] 
         for lint in CHECKS:
             sym = lint['symbol']
             should_check = getattr(bpy.context.scene, lint['check_prop'])
@@ -85,8 +85,19 @@ class MeshLintAnalyzer:
                 indices = bad.get(elemtype, [])
                 report[elemtype] = indices
                 lint['count'] += len(indices)
-            found.append(report)
-        return found
+            analysis.append(report)
+        return analysis
+
+    @classmethod
+    def none_analysis(cls):
+        analysis = []
+        for lint in CHECKS:
+            analysis.append({
+                'lint': lint,
+                'verts': [],
+                'edges': [],
+                'faces': []})
+        return analysis
 
     def ensure_edit_mode(self):
         if 'EDIT_MESH' != bpy.context.mode:
@@ -166,28 +177,50 @@ def repeated_check(dummy):
         return
     analyzer = MeshLintAnalyzer()
     now_counts = analyzer.topology_counts()
+    if not None is previous_topology_counts:
+        previous_data_name = previous_topology_counts['data'].name
+    else:
+        previous_data_name = None
+    now_name = now_counts['data'].name
     if None is previous_topology_counts \
             or now_counts != previous_topology_counts:
+        data_names_same = previous_data_name == now_name
+        if not data_names_same:
+            print("TODO: Verify that this functionality works then remove this message.")
+            before = MeshLintAnalyzer.none_analysis()
         analysis = analyzer.find_problems()
-        for i in range(len(analysis)):
-            prev = previous_analysis[i]
-            now = analysis[i]
-            if None is prev:
-                continue
-            for elemtype in 'verts', 'edges', 'faces': # XXX redundant
-                if len(now[elemtype]) > len(prev[elemtype]):
-                    print(prev)
-                    print(now)
-                    print('----SOMETHING IS NOT WORKING... =(-----')
-                
+        diff_msg = diff_analyses(analysis, previous_analysis)
+        # print(diff_msg) # TODO - make a happy message thing.
         previous_topology_counts = now_counts
         previous_analysis = analysis
-    
+
+
+def diff_analyses(before, analysis):
+    if None is before:
+        before = MeshLintAnalyzer.none_analysis()
+    report_strings = []
+    for i in range(len(analysis)):
+        report_before = before[i]
+        report = analysis[i]
+        check_elem_strings = []
+        for elemtype in 'verts', 'edges', 'faces': # XXX redundant
+            elem_list_before = report_before[elemtype]
+            elem_list = report[elemtype]
+            if len(elem_list) > len(elem_list_before):
+                count_diff = len(elem_list) - len(elem_list_before)
+                elem_string = depluralize(count=count_diff, string=elemtype)
+                check_elem_strings.append(str(count_diff) + ' ' + elem_string)
+        if len(check_elem_strings):
+            report_strings.append(
+                report['lint']['label'] + ': ' + ', '.join(check_elem_strings))
+    if len(report_strings):
+        return 'MeshLint found ' + ', '.join(report_strings)
+    return None
 
 is_live_global = False
 
 class MeshLintVitalizer(bpy.types.Operator):
-    'Toggles the real-time execution of the checks.'
+    'Toggles the real-time execution of the checks'
     bl_idname = 'meshlint.live_toggle'
     bl_label = 'MeshLint Live Toggle'
 
@@ -206,7 +239,7 @@ class MeshLintVitalizer(bpy.types.Operator):
         return {'FINISHED'}
 
 class MeshLintSelector(bpy.types.Operator):
-    'Uncheck boxes below to prevent those checks from running.'
+    'Uncheck boxes below to prevent those checks from running'
     bl_idname = 'meshlint.select'
     bl_label = 'MeshLint Select'
 
@@ -255,15 +288,15 @@ class MeshLintControl(bpy.types.Panel):
         left.operator(
             'meshlint.select', text='Select Lint', icon='EDITMODE_HLT')
 
-        # right = split.column()
-        # global is_live_global
-        # if is_live_global:
-            # live_label = 'Pause Checking...'
-            # play_pause = 'PAUSE'
-        # else:
-            # live_label = 'Continuous Check!'
-            # play_pause = 'PLAY'
-        # right.operator('meshlint.live_toggle', text=live_label, icon=play_pause)
+        right = split.column()
+        global is_live_global
+        if is_live_global:
+            live_label = 'pause checking...'
+            play_pause = 'PAUSE'
+        else:
+            live_label = 'Continuous Check!'
+            play_pause = 'PLAY'
+        right.operator('meshlint.live_toggle', text=live_label, icon=play_pause)
 
     def add_rows(self, layout, context):
         col = layout.column()
@@ -271,6 +304,7 @@ class MeshLintControl(bpy.types.Panel):
         if not has_active_mesh(context):
             return
         for lint in CHECKS:
+            # TODO - switch back to the big buttons. The checkboxes are lame.
             count = lint['count']
             if count in (TBD_STR, N_A_STR):
                 label = str(count) + ' ' + lint['label']
@@ -280,8 +314,7 @@ class MeshLintControl(bpy.types.Panel):
                 reward = 'SOLO_ON'
             else:
                 label = str(count) + 'x ' + lint['label']
-                if 1 == count:
-                    label = label.rstrip('s')
+                label = depluralize(count=count, string=label)
                 reward = 'ERROR'
             row = col.row()
             row.prop(context.scene, lint['check_prop'], text='')
@@ -317,8 +350,15 @@ class MeshLintControl(bpy.types.Panel):
             'SurfTorus',
             'Text',
         ]
-        pat = '(%s).?\d*$' % '|'.join(default_names)
+        pat = '(%s)\.?\d*$' % '|'.join(default_names)
         return re.match(pat, name)
+
+
+def depluralize(**args):
+    if 1 == args['count']:
+        return args['string'].rstrip('s')
+    else:
+        return args['string']
            
 
 def register():
@@ -329,7 +369,139 @@ def unregister():
     bpy.utils.unregister_module(__name__)
 
 
+import unittest
+import warnings
+import time
+
+class TestUtilities(unittest.TestCase):
+    def test_depluralize(self):
+        self.assertEqual(
+            'foo',
+            depluralize(count=1, string='foos'))
+        self.assertEqual(
+            'foos',
+            depluralize(count=2, string='foos'))
+
+
+class TestAnalysis(unittest.TestCase):
+    def test_comparison(self):
+        a = MeshLintAnalyzer.none_analysis()
+        b = MeshLintAnalyzer.none_analysis()
+        self.assertEqual(
+            None,
+            diff_analyses(
+                MeshLintAnalyzer.none_analysis(),
+                MeshLintAnalyzer.none_analysis()),
+            'Two none_analysis()s')
+        self.assertEqual(
+            "MeshLint found SomeCheck: 4 verts",
+            diff_analyses(
+                None,
+                [
+                    {
+                        'lint': { 'label': 'SomeCheck' },
+                        'verts': [1,2,3,4],
+                        'edges': [],
+                        'faces': [],
+                    },
+                ]),
+            'When there was no previous analysis')
+        self.assertEqual(
+            "MeshLint found CheckA: 2 edges, CheckC: 4 verts, 1 face",
+            diff_analyses(
+                [
+                    { 'lint': { 'label': 'CheckA' },
+                      'verts': [], 'edges': [1,4], 'faces': [], },
+                    { 'lint': { 'label': 'CheckB' },
+                      'verts': [], 'edges': [2,3], 'faces': [], },
+                    { 'lint': { 'label': 'CheckC' },
+                      'verts': [], 'edges': [], 'faces': [2,3], },
+                ],
+                [
+                    { 'lint': { 'label': 'CheckA' },
+                      'verts': [], 'edges': [1,4,5,6], 'faces': [], },
+                    { 'lint': { 'label': 'CheckB' },
+                      'verts': [], 'edges': [2,3], 'faces': [], },
+                    { 'lint': { 'label': 'CheckC' },
+                      'verts': [1,2,3,4], 'edges': [], 'faces': [2,3,5], },
+                ]),
+            'Complex comparison of analyses')
+
+class QuietOnSuccessTestresult(unittest.TextTestResult):
+    def startTest(self, test):
+        pass
+
+    def addSuccess(self, test):
+        pass
+
+
+class QuietTestRunner(unittest.TextTestRunner):
+    resultclass = QuietOnSuccessTestresult
+
+    def run(self, test):
+        # Ugh. I really shouldn't have to include this much code, but they
+        # left it so unrefactored I don't know what else to do. - rking
+        "Run the given test case or test suite."
+        result = self._makeResult()
+        unittest.registerResult(result)
+        result.failfast = self.failfast
+        result.buffer = self.buffer
+        with warnings.catch_warnings():
+            if self.warnings:
+                # if self.warnings is set, use it to filter all the warnings
+                warnings.simplefilter(self.warnings)
+                # if the filter is 'default' or 'always', special-case the
+                # warnings from the deprecated unittest methods to show them
+                # no more than once per module, because they can be fairly
+                # noisy.  The -Wd and -Wa flags can be used to bypass this
+                # only when self.warnings is None.
+                if self.warnings in ['default', 'always']:
+                    warnings.filterwarnings('module',
+                            category=DeprecationWarning,
+                            message='Please use assert\w+ instead.')
+            startTime = time.time()
+            startTestRun = getattr(result, 'startTestRun', None)
+            if startTestRun is not None:
+                startTestRun()
+            try:
+                test(result)
+            finally:
+                stopTestRun = getattr(result, 'stopTestRun', None)
+                if stopTestRun is not None:
+                    stopTestRun()
+            stopTime = time.time()
+        timeTaken = stopTime - startTime
+        result.printErrors()
+        run = result.testsRun
+
+        expectedFails = unexpectedSuccesses = skipped = 0
+        try:
+            results = map(len, (result.expectedFailures,
+                                result.unexpectedSuccesses,
+                                result.skipped))
+        except AttributeError:
+            pass
+        else:
+            expectedFails, unexpectedSuccesses, skipped = results
+
+        infos = []
+        if not result.wasSuccessful():
+            self.stream.write("FAILED")
+            failed, errored = len(result.failures), len(result.errors)
+            if failed:
+                infos.append("failures=%d" % failed)
+            if errored:
+                infos.append("errors=%d" % errored)
+        if skipped:
+            infos.append("skipped=%d" % skipped)
+        if expectedFails:
+            infos.append("expected failures=%d" % expectedFails)
+        if unexpectedSuccesses:
+            infos.append("unexpected successes=%d" % unexpectedSuccesses)
+        return result
+
 if __name__ == '__main__':
+    unittest.main(testRunner=QuietTestRunner, argv=['dummy'], exit=False)
     register()
 
 # vim:ts=4 sw=4 sts=4
